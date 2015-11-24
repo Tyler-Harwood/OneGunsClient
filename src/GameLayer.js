6001d041aacddbe32e819a4679657246968831eb
Tyler-Harwood <andrewtharwood@gmail.com>
2015-11-23 21:41:27 -0500

    Moved things over to the seperate class and got things working :)

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,35 +1,125 @@
 var GameLayer = cc.Layer.extend({
-    backgroundLayer:null,
-    textLayer:null,
-    width:null,
-    height:null,
-    scale:null,
-    pos:null,
-    meh:null,
-    originalPosition:null,
-    startPosition:null,
-    currentPosition:null,
-    tileText:null,
-    mousePosition:null,
-    mouseScroll:null,
-    ctor: function () {
+    backgroundLayer: null,
+    textLayer: null,
+    tileWidth: 0,
+    tileHeight: 0,
+    currentScale: null,
+    pos: null,
+    meh: null,
+    originalPosition: null,
+    startPosition: null,
+    currentPosition: null,
+    tileText: null,
+    mousePosition: null,
+    mouseScroll: null,
+    ctor: function (space) {
         this._super();
-        this.initWithFile("assets/target.png");
 
     },
     init: function () {
-        backgroundLayer;
-        textLayer;
-        width = 20;
-        height = 20;
-        scale = 1;
-        pos = cc.p(0, 0);
-        meh;
-        originalPosition = cc.p(0, 0);
-        startPosition = cc.p(0, 0);
-        currentPosition = cc.p(0, 0);
-        tileText;
-        mousePosition = cc.p(0, 0);
-        mouseScroll = 1;
+        this._super();
+
+        this.tileWidth = 20;
+        this.tileHeight = 20;
+        this.currentScale = 1;
+        this.pos = cc.p(0, 0);
+        this.originalPosition = cc.p(0, 0);
+        this.startPosition = cc.p(0, 0);
+        this.currentPosition = cc.p(0, 0);
+        this.mousePosition = cc.p(0, 0);
+        this.mouseScroll = 1;
+        this.backgroundLayer = cc.LayerColor.create(new cc.Color(40, 40, 40, 255), 480, 480);
+        this.addChild(this.backgroundLayer);
+
+        var tileMap = new Array(this.tileWidth);
+
+        for (var i = 0; i < this.tileWidth; i++) {
+            tileMap[i] = new Array(this.tileHeight);
+        }
+
+        for (var x = 0; x < this.tileWidth; x++) {
+            for (var y = 0; y < this.tileHeight; y++) {
+                tileMap[x][y] = new BackgroundTile();
+                this.backgroundLayer.addChild(tileMap[x][y], 0);
+                tileMap[x][y].setPosition(x * spriteDimension, y * spriteDimension);
+            }
+        }
+
+        this.meh = new TestPlayer();
+
+        this.textLayer = cc.Layer.create();
+
+        this.backgroundLayer.addChild(this.meh, 1);
+        this.backgroundLayer.setAnchorPoint(cc.p(0, 0));
+
+        this.tileText = cc.LabelTTF.create("test", "Arial", "32", cc.TEXT_ALIGNMENT_LEFT);
+        this.textLayer.addChild(this.tileText);
+        this.textLayer.setPosition(100, 20);
+        this.addChild(this.textLayer);
+
+        cc.eventManager.addListener({
+            event: cc.EventListener.TOUCH_ALL_AT_ONCE,
+            onTouchesBegan: this.onTouchesBegan,
+            onTouchesMoved: this.onTouchesMoved,
+            onTouchesEnded: this.onTouchesEnded
+        }, this);
+
+        cc.eventManager.addListener({
+            event: cc.EventListener.MOUSE,
+            onMouseMove: this.onMouseMove,
+            onMouseScroll: this.onMouseScroll
+        }, this);
+
+        this.scheduleUpdate();
+    },
+    onTouchesBegan: function (touches, event) {
+        var touch = touches[0];
+        var test = event.getCurrentTarget();
+        test.pos = touch.getLocation();
+        test.meh.setPosition(test.backgroundLayer.convertToNodeSpaceAR(test.pos));//(test.pos.x - backgroundLayer.x ) / test.currentScale, (test.pos.y - backgroundLayer.y) / test.currentScale);
+        test.startPosition = test.pos;
+
+        return true;
+    },
+    onTouchesMoved: function (touches, event) {
+        var touch = touches[0];
+        var test = event.getCurrentTarget();
+        //if (lastMovePosition == null) {
+        test.currentPosition = touch.getLocation();
+        test.backgroundLayer.setPosition(test.originalPosition.x + test.currentPosition.x - test.startPosition.x, test.originalPosition.y + test.currentPosition.y - test.startPosition.y);
+        //}
+        //this.pos = touch.getLocation();
+        //var test = touch.getLocation()
+        //this.setPosition(test);
+
+    },
+    onTouchesEnded: function (touches, event) {
+        var touch = touches[0];
+        var test = event.getCurrentTarget();
+        test.originalPosition = test.backgroundLayer.getPosition();
+        //this.pos = touch.getLocation();
+        //meh.setPosition(this.pos);
+    },
+    onMouseMove: function (event) {
+        var test = event.getCurrentTarget();
+        test.mousePosition = event.getLocation();
+        var text = test.getCurrentTilePosition(test.mousePosition.x, test.mousePosition.y);
+        test.tileText.setString(text);
+    },
+    onMouseScroll: function (event) {
+        var test = event.getCurrentTarget();
+        test.mouseScroll = event.getScrollY();
+        test.backgroundLayer.setScale(test.currentScale += (test.mouseScroll / 1000));
+    },
+    getCurrentTilePosition: function (x, y) {
+        var backgroundPosition = this.backgroundLayer.convertToNodeSpaceAR(cc.p(x, y));
+
+        var xPos = Math.floor(backgroundPosition.x / spriteDimension);
+        var yPos = Math.floor(backgroundPosition.y / spriteDimension);
+
+        return "Pos: " + xPos + ", " + yPos;
+    },
+    update: function (dt) {
+        var test = 1;
     }
 });
--- src/app.js
+++ src/app.js
@@ -1,115 +1,89 @@
 var gameScene = cc.Scene.extend({
     onEnter: function () {
         this._super();
-        var gameLayer = new game();
+        var gameLayer = new GameLayer();
         gameLayer.init();
         this.addChild(gameLayer);
     }
 });
 
-var backgroundLayer;
-var textLayer;
-var width = 20;
-var height = 20;
-var scale = 1;
-var pos = cc.p(0,0);
-var meh;
-var originalPosition = cc.p(0,0);
-var startPosition = cc.p(0,0);
-var currentPosition = cc.p(0,0);
-var tileText;
-var mousePosition = cc.p(0,0);
-var mouseScroll = 1;
+//var backgroundLayer;
+//var textLayer;
+//var width = 20;
+//var height = 20;
+//var scale = 1;
+//var pos = cc.p(0,0);
+//var meh;
+//var originalPosition = cc.p(0,0);
+//var startPosition = cc.p(0,0);
+//var currentPosition = cc.p(0,0);
+//var tileText;
+//var mousePosition = cc.p(0,0);
+//var mouseScroll = 1;
 
 const spriteDimension = 32;
 
-var getCurrentTilePosition = function (x, y) {
-    var backgroundPosition = backgroundLayer.convertToNodeSpaceAR(cc.p(x,y));
+//var getCurrentTilePosition = function (x, y) {
+//    var backgroundPosition = backgroundLayer.convertToNodeSpaceAR(cc.p(x,y));
+//
+//    var xPos = Math.floor(backgroundPosition.x / spriteDimension);
+//    var yPos = Math.floor(backgroundPosition.y / spriteDimension);
+//
+//    return "Pos: " + xPos + ", " + yPos;
+//};
 
-    var xPos = Math.floor(backgroundPosition.x / spriteDimension);
-    var yPos = Math.floor(backgroundPosition.y / spriteDimension);
-
-    return "Pos: " + xPos + ", " + yPos;
-};
+var test;
 
 var game = cc.Layer.extend({
     init: function () {
         this._super();
-        backgroundLayer = cc.LayerColor.create(new cc.Color(40, 40, 40, 255), 480, 480);
-        this.addChild(backgroundLayer);
 
-        var tileMap = new Array(width);
+        test = new GameLayer;
 
-        for (var i = 0; i < width; i++) {
-            tileMap[i] = new Array(height);
-        }
-
-        for (var x = 0; x < width; x++) {
-            for (var y = 0; y < height; y++) {
-                tileMap[x][y] = new BackgroundTile();
-                backgroundLayer.addChild(tileMap[x][y], 0);
-                tileMap[x][y].setPosition(x * spriteDimension, y * spriteDimension);
-            }
-        }
-
-        meh = new TestPlayer();
-
-        textLayer = cc.Layer.create();
-
-        backgroundLayer.addChild(meh, 1);
-        backgroundLayer.setAnchorPoint(cc.p(0,0));
-
-        tileText = cc.LabelTTF.create("test", "Arial", "32", cc.TEXT_ALIGNMENT_LEFT);
-        textLayer.addChild(tileText);
-        textLayer.setPosition(100, 20);
-        this.addChild(textLayer);
-
-        cc.eventManager.addListener({
-            event: cc.EventListener.TOUCH_ONE_BY_ONE,
-            swallowTouches: true,
-            onTouchBegan: this.onTouchBegan,
-            onTouchMoved: this.onTouchMoved,
-            onTouchEnded: this.onTouchEnded
-        }, this);
-
-        cc.eventManager.addListener({
-            event: cc.EventListener.MOUSE,
-            onMouseMove: this.onMouseMove,
-            onMouseScroll: this.onMouseScroll
-        }, this);
-
-        this.scheduleUpdate();
-    },
-    update: function (dt) {
-    },
-    onTouchBegan: function (touch, event) {
-        pos = touch.getLocation();
-        meh.setPosition(backgroundLayer.convertToNodeSpaceAR(pos));//(pos.x - backgroundLayer.x ) / scale, (pos.y - backgroundLayer.y) / scale);
-        startPosition = pos;
-
-        return true;
-    },
-    onTouchMoved: function (touch, event) {
-        //if (lastMovePosition == null) {
-        currentPosition = touch.getLocation();
-        backgroundLayer.setPosition(originalPosition.x + currentPosition.x - startPosition.x, originalPosition.y + currentPosition.y - startPosition.y);
+        this.addChild(test);
+        //backgroundLayer = cc.LayerColor.create(new cc.Color(40, 40, 40, 255), 480, 480);
+        //this.addChild(backgroundLayer);
+        //
+        //var tileMap = new Array(width);
+        //
+        //for (var i = 0; i < width; i++) {
+        //    tileMap[i] = new Array(height);
         //}
-        //pos = touch.getLocation();
-        //var test = touch.getLocation()
-        //this.setPosition(test);
-
-    },
-    onTouchEnded: function (touch, event) {
-        originalPosition = backgroundLayer.getPosition();
-        //pos = touch.getLocation();
-        //meh.setPosition(pos);
-    },
-    onMouseMove: function (event) {
-        mousePosition = event.getLocation();
-        tileText.setString(getCurrentTilePosition(mousePosition.x, mousePosition.y));
-    },
-    onMouseScroll: function (event) {
-        mouseScroll = event.getScrollY();
-        backgroundLayer.setScale(scale += (mouseScroll / 1000));
+        //
+        //for (var x = 0; x < width; x++) {
+        //    for (var y = 0; y < height; y++) {
+        //        tileMap[x][y] = new BackgroundTile();
+        //        backgroundLayer.addChild(tileMap[x][y], 0);
+        //        tileMap[x][y].setPosition(x * spriteDimension, y * spriteDimension);
+        //    }
+        //}
+        //
+        //meh = new TestPlayer();
+        //
+        //textLayer = cc.Layer.create();
+        //
+        //backgroundLayer.addChild(meh, 1);
+        //backgroundLayer.setAnchorPoint(cc.p(0,0));
+        //
+        //tileText = cc.LabelTTF.create("test", "Arial", "32", cc.TEXT_ALIGNMENT_LEFT);
+        //textLayer.addChild(tileText);
+        //textLayer.setPosition(100, 20);
+        //this.addChild(textLayer);
+        //
+        //cc.eventManager.addListener({
+        //    event: cc.EventListener.TOUCH_ONE_BY_ONE,
+        //    swallowTouches: true,
+        //    onTouchBegan: this.onTouchBegan,
+        //    onTouchMoved: this.onTouchMoved,
+        //    onTouchEnded: this.onTouchEnded
+        //}, this);
+        //
+        //cc.eventManager.addListener({
+        //    event: cc.EventListener.MOUSE,
+        //    onMouseMove: this.onMouseMove,
+        //    onMouseScroll: this.onMouseScroll
+        //}, this);
+        //
+        //this.scheduleUpdate();
     }
 });
